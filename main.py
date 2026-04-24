7150dd53f716982f769b3614b85a87de5ab401b1
Thenmozhi Mathichelvan <thenmj1492@gmail.com>
2026-04-24 12:36:20 +0530

    Update success message in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
 VERSION = "2.0.0"
 while True:
-    print(f"I AM THE NEW VERSION {VERSION}! THE UPDATE WORKED! HORRAYYYYYYYY")
+    print(f"I AM THE NEW VERSION {VERSION}! THE UPDATE WORKED! Automatic success")
     time.sleep(5)
