c054000a2fd94b07e47d743e1a060303aaccb0f4
Thenmozhi Mathichelvan <thenmj1492@gmail.com>
2026-04-24 11:52:03 +0530

    Modify output message in main.py
    Updated the print statement to include 'HORRAYYYYYYYY'.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
 VERSION = "2.0.0"
 while True:
-    print(f"I AM THE NEW VERSION {VERSION}! THE UPDATE WORKED!")
+    print(f"I AM THE NEW VERSION {VERSION}! THE UPDATE WORKED! HORRAYYYYYYYY")
     time.sleep(5)
